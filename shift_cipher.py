80e1fe920ba17fc0fb4688f0eb99b864cc6f7b4d
davehrtng <david.e.harting@gmail.com>
2016-08-05 18:03:17 -0400

    reformat docstring

--- shift_cipher.py
+++ shift_cipher.py
@@ -4,10 +4,8 @@ class ShiftCipher:
         """
         Encrypts the text by shifting each character "up" the alphabet by the amount specified by key.
 
-        Inputs
-            text: the text to encrypt
-            key: each alphabet character of text will be shifted up by this amount
-        Outputs
-            the encrypted message
+        :param text: the text to encrypt
+        :param key: each alphabet character of text will be shifted up by this amount
+        :return: the encrypted message
         """
         return text;
