eb4c10fac7a077fbd86c5f5fc90881fd921d3a0d
davehrtng <david.e.harting@gmail.com>
2016-08-06 14:27:21 -0400

    add decryption to main to demonstrate full functionality

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from shift_cipher import ShiftCipher
 
 def main():
-    message = "This is the raw message full of secrets."
+    message = "This is the clear text message full of secrets."
+    key = 15
+    print(message)
+
+    print("... Encrypting ...")
     encrypted_message = ShiftCipher.encrypt(message, 5)
     print(encrypted_message)
+
+    print("... Decrypting ...")
+    decrypted_message = ShiftCipher.decrypt(encrypted_message, 5)
+    print(decrypted_message)
 
 if __name__ == "__main__":
     main()
